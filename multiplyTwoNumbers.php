22ea4a269b3485861c6331c4a5bb66e9d1a6db3b
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-07-10 20:08:36 +0600

    Show the multiplication of two numbers.

---
+++ multiplyTwoNumbers.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$firstNumber = 10;
+$secondNumber = 20;
+
+$multiply = $firstNumber*$secondNumber;
+
+echo 'The multiplication of two numbers is:'.$multiply;
+
+
+?>
+
