efe3797bae0b6505fd6c14ced783d89ff7254dd6
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-07-11 10:53:29 +0600

    Exercise fo foreach loop.

---
+++ forEachLoop.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ Print the value of an Prdefined Array
+ Written by Mainul Hasan
+ */
+
+$someNumber = array(8,5,6,7);
+
+foreach ($someNumber as $anotherArray)
+{
+    echo $anotherArray." ";
+}
+?>
+
