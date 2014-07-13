9504d0fe911e6218a944a0e388ceb070246d8084
shuvrow <shuvrow@gmail.com>
2014-07-13 08:48:03 +0600

    Show the give input with id,name,salary.

---
+++ showSalary.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <bod>
+        <?php
+            $id = '007';
+            $name = 'James bond';
+            $salary = '10000';
+            echo $id.' ',$name.' ',$salary;
+        ?>
+    </body>
+    
+</html>
+
+
