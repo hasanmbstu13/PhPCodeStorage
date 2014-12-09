989d0652fde4286c3157161dce67102b42f01ea3
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-12-09 08:20:33 +0600

    Working with most common array functions like count(),max(),min(),implode(),explode() etc.

--- PHP-Rookie/ArrayFunctions.php
+++ PHP-Rookie/ArrayFunctions.php
@@ -13,7 +13,11 @@
 	</pre>
 	<?php echo $myArray[2]['name']; ?> -->
 
-<?php $numbers = [1,3,4,5,45,2,345]; ?>
+<?php 
+
+	$numbers = array(1,3,4,5,45,2,345);
+	echo $numbers[0]; 
+?>
 
 Count: 	   <?php echo count($numbers); ?><br/>
 Max value: <?php echo max($numbers); ?><br/>
@@ -22,10 +26,26 @@ Min value: <?php echo min($numbers); ?><br/>
 <br/>
 
 <pre>
-Sort: 			<?php sort($numbers);  print_r($numbers); //Sort()-->Sort the array in ascending order?><br/>
-Reverse sort: 	<?php rsort($numbers); print_r($numbers); //Rsort()-->Sort the array in descending order?><br/>
+Sort: 			<?php sort($numbers);  print_r($numbers); //Sort()Sort the array in ascending order?><br/>
+Reverse sort: 	<?php rsort($numbers); print_r($numbers); //Rsort()Sort the array in descending order?><br/>
 </pre>
 <br/>
+
+Implode:<?php   
+//implode turn an array into string In other words combined values together to get a string
+echo $num_string = implode(" * ", $numbers);
+?>
+
+<br/>
+
+Explode: <?php print_r(explode(" * ", $num_string)); 
+//Explode() takes a string every time it takes the user divider as a new object in array
+//Explode split the string in divider place the object as a new element in array
+//Explode is extremly useful in comma separtor list ?>
+<br/>
+
+5 in array?: 	<?php echo in_array(5, $numbers);//returns T/F ?><br/>
+19in array?:    <?php echo in_array(19, $numbers); //returns T/F?><br/>
 
 </body>
 </html>
