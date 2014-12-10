b331ee499318b708a651bac6774eaedef2517ecf
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-12-10 08:32:36 +0600

    Its not a best practice to depend on type juggling.

---
+++ PHP-Rookie/type_casting.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Type Juggling and Type Casting</title>
+</head>
+<body>
+	Type Juggling<br/>
+	<?php $count = "2 peoples"; ?>
+	Type: <?php echo gettype($count); ?><br/>
+
+	<?php $count += 3; //data type automatically changes from string to integer?>
+	Type: <?php echo gettype($count); ?><br>
+
+	<?php $cats = "I have" . $count . "cats.";////data type automatically changes from integer to string ?>
+	Cats: <?php echo gettype($cats); ?><br>
+	<br>
+	<?php //Its not a best practice to depend on type juggling ?>
+
+
+</body>
+</html>
