05ce2015c48fbf1f97219e8aadf815aedfcda4f0
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-12-10 08:00:20 +0600

    isset() function determines whether a variable is set or not.

--- PHP-Rookie/NULL.php
+++ PHP-Rookie/NULL.php
@@ -12,5 +12,9 @@
 	 var1 null? <?php echo is_null($var1); ?><br/>
 	 var2 null? <?php echo is_null($var2); ?><br/>
 	 var3 null? <?php echo is_null($var3); ?><br/>
+	 <br/>
+	 var1 is set? <?php echo isset($var1); ?><br/>
+	 var2 is set? <?php echo isset($var2); ?><br/>
+	 var3 is set? <?php echo isset($var3); ?><br/>
 </body>
 </html>
