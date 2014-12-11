8d0ca38ad2716115bd8c9145536227548d688fd5
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-12-11 08:12:43 +0600

    Settype() and Gettype() are used for type casting by default.

--- PHP-Rookie/type_casting.php
+++ PHP-Rookie/type_casting.php
@@ -15,8 +15,12 @@
 	<?php $cats = "I have" . $count . "cats.";////data type automatically changes from integer to string ?>
 	Cats: <?php echo gettype($cats); ?><br>
 	<br>
-	<?php //Its not a best practice to depend on type juggling ?>
 
+	<?php //Its not a best practice to depend on type juggling ?>
+	
+	Type Casting <br>
+	<?php settype($count, "integer") ?>
+	Count: <?php echo gettype($count); ?>
 
 </body>
 </html>
