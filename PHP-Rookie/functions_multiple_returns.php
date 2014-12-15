ec31578b576d76cbf99bee4414012e58339a3087
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-12-15 22:50:56 +0600

    Default argument is used when we define the function.

--- PHP-Rookie/functions_multiple_returns.php
+++ PHP-Rookie/functions_multiple_returns.php
@@ -15,12 +15,20 @@
 		}
 
 		$result_array = add_subt(10,5);
+
 		echo "Add: " . $result_array[0] . "<br />";
 		echo "Subt: " .$result_array[1]  . "<br/>";
 
 		list($add_result, $subt_result) = add_subt(20,7);//list is the powerful tool to pack a variable and immediately unpack the variable.
+		
 		echo "Add: " . $add_result . "<br />";
 		echo "Subt: " . $subt_result . "<br />";
 	 ?>
+
+<!-- 	 <pre>
+	<?php 
+		print_r($result_array);
+	 ?>
+</pre> -->
 </body>
 </html>
