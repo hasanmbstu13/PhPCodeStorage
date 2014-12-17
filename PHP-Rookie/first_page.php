bcf20d1051e9f693508102c917a05d9379380af7
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-12-17 08:15:12 +0600

    We can make a static page in dynamic by using php. Query string is used to send huge amount of data.

---
+++ PHP-Rookie/first_page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>First Page</title>
+</head>
+<body>
+
+	<?php $link_name = "Second Page"; ?>
+	<?php $id = 2; ?>
+
+	<a href="second_page.php?id=<?php echo $id;?>"><?php echo $link_name; ?></a>
+
+</body>
+</html>
