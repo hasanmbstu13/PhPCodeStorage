99beb035d13b8892886898615b390a866fcc9486
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-01-04 22:39:34 +0600

    Practice different types of validation and see validation errors.

--- PHP-Rookie/form_processing.php
+++ PHP-Rookie/form_processing.php
@@ -11,6 +11,7 @@
 		 ?>
 	</pre>
 	<?php 
+		//isset() is used to determine whether a variable is set or not.
 		if (isset($_POST["userName"])) {
 			$username = $_POST["userName"];
 		}else{
@@ -20,7 +21,17 @@
 			$password = $_POST["password"];
 		}else{
 			$password = "";
-		}	
+		}
+	?>
+
+	<?php
+		//set default values using ternary operator 
+	   //boolean_test ? value_if_true : value_if_false
+		$username = isset($_POST['userName']) ? $_POST['userName'] : "";
+		$password = isset($_POST['password']) ? $_POST['password'] : "";
+	 ?>
+	 
+	<?php	
 		echo "{$username}: {$password}";
 	 ?>
 </body>
--- PHP-Rookie/forms.php
+++ PHP-Rookie/forms.php
@@ -5,20 +5,19 @@
 	<title>Form</title>
 </head>
 <body>
-
+<!-- 
 <?php 
 
 	$num = 1;
 	$num1 = 2;
 	echo "3 house+" + $num1;
  ?>
-	
-<!-- 	<form action="form_processing.php" method = "post">
+-->
+<form action="form_processing.php" method = "post">
 	Username:<input type="text" name="userName" value=""><br>
 	Password:<input type="password" name="password" value=""><br>
 	<br>
 	<input type="submit" name="submit" value="Submit!">
 </form>
- -->
 </body>
 </html>
