1c4f8a211a08a44c9fc75f7fabbe67ad9cd1a3ae
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-05-09 06:03:33 +0600

    Develop a project names widget_Corp

---
+++ Widget_Corp/includes/layouts/footer.php
@@ -0,0 +1,9 @@
+<div id="footer">Copyright 2007, Widget Corp</div>
+</body>
+</html>
+<?php 
+ 	// 5. Close database connection
+	if ($connection) {
+		mysqli_close($connection);
+	}	
+?> 
---
+++ Widget_Corp/includes/layouts/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Widget Corp</title>
+	<link rel="stylesheet" type="text/css" href="stylesheets/public.css">
+	<!-- <meta charset="UTF-8"> -->
+</head>
+<body>
+	<div id="header">
+		<h1>Widget Corp</h1>
+	</div>
