8399e84fddf68de8c4c87906cae4a3a64dc1a9f8
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-05-09 08:45:20 +0600

    Refactoring is helpful for re-organized the code in more better way.

---
+++ Widget_Corp/navigation_bar_in_simple_html_format.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Unorderd List</title>
+</head>
+<body>
+	<ul>
+		<li>
+			Abour Widget Corp
+			<ul>
+				<li>Our Mission</li>
+				<li>Our History</li>
+			</ul>
+		</li>
+		<li>
+			Abour Widget Corp
+			<ul>
+				<li>Our Mission</li>
+				<li>Our History</li>
+			</ul>
+		</li>
+	</ul>
+</body>
+</html>
--- Widget_Corp/public/manage_content.php
+++ Widget_Corp/public/manage_content.php
@@ -1,42 +1,72 @@
 <?php require_once("../includes/db_connection.php") ?>
 <?php require_once("../includes/functions.php") ?>
-<?php 
- 	//2. Perform database query
-$query  = "SELECT * ";  
-$query .= "FROM subjects ";  
-$query .= "WHERE visible = 1 ";
-$query .= "ORDER BY position ASC";
 
-$result = mysqli_query($connection, $query); 
- 	//Test if there was a query error
-confirm_query($result);
-?>
 
 <?php include("../includes/layouts/header.php") ?>
 
 <div id="main">
 	<div id="navigation">
-		<ul class="subjects">
-			<?php 
- 		//3. Use returned data (if any)
- 		while($subject = mysqli_fetch_assoc($result)){ //use 
- 			//output data from each row
- 			?>
- 			<li><?php echo $subject["menu_name"]."(".$subject["id"].")"; ?></li>
- 			<?php
- 		}
- 		?>
- 	</ul>
- </div>
- <div id="page">
- 	<h2>Manage Content</h2>
- </div>
-</div>
+			<ul class="subjects">
 
-<?php 
-//4. Release returned data
-mysqli_free_result($result);
-?>
+				<?php 
+					//2. Perform database query
+					$query  = "SELECT * ";  
+					$query .= "FROM subjects ";  
+					$query .= "WHERE visible = 1 ";
+					$query .= "ORDER BY position ASC";
+
+					$subject_set = mysqli_query($connection, $query); 
+					//Test if there was a query error
+					confirm_query($subject_set);
+				?>
+
+				<?php 
+	 				//3. Use returned data (if any)
+	 				while($subject = mysqli_fetch_assoc($subject_set)){ //use 
+	 				//output data from each row
+	 			?>
+				<li>
+	 				<?php echo $subject["menu_name"]."(".$subject["id"].")"; ?>
+	 				
+	 				<?php 
+						//2. Perform database query
+						$query  = "SELECT * ";  
+						$query .= "FROM pages ";  
+						$query .= "WHERE visible = 1 ";
+						$query .= "AND subject_id = {$subject["id"]} ";
+						$query .= "ORDER BY position ASC";
+
+						$page_set = mysqli_query($connection, $query); 
+						//Test if there was a query error
+						confirm_query($page_set);
+					?>
+
+		 			<ul class="pages">
+		 				<?php 
+			 				while($page = mysqli_fetch_assoc($page_set)){ 
+	 					?>
+	 							<li><?php echo $page["menu_name"]; ?></li>
+		 					<?php
+		 					  }
+		 					?>
+		 					<?php mysqli_free_result($page_set); ?>
+		 			</ul>
+		 		</li>
+
+		 		<?php
+		 			}
+		 		?>
+
+				<?php 
+					//4. Release returned data
+					mysqli_free_result($subject_set);
+				?>
+	 	</ul>
+ </div>
+<div id="page">
+ 	<h2>Manage Content</h2>
+</div>
+</div>
 
 <?php include("../includes/layouts/footer.php") ?> 
 //here .. means parent directory and . means previous directory
