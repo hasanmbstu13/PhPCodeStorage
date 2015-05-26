f41046e3b9a76a4ab452d9f4fcf3dca926fdbcd2
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-05-26 08:05:36 +0600

    Refactor the navigation bar.

--- Widget_Corp/includes/functions.php
+++ Widget_Corp/includes/functions.php
@@ -66,16 +66,31 @@
 			return null;
 		}
 	}
+	function find_selected_page(){
+		 global $current_subject;
+		 global $current_page;
+		 
+		 if(isset($_GET["subject"])){
+		  $current_subject = find_subject_by_id($_GET["subject"]);
+		  $current_page = null;	
+		}elseif (isset($_GET["page"])) {
+		  $current_page = find_page_by_id($_GET["page"]);
+		  $current_subject = null;
+		}else{
+		  $current_page = null;  
+		  $current_subject = null;
+		}
+	}
 
 		 //navigation takes 2 arguments
-		// - the currently selected subject ID (if any)
-	   // - the currently selected page ID (if any)
-	function navigation($subject_id, $page_id){
+		// - the current subject array or null
+	   // - the current page array or null
+	function navigation($subject_array, $page_array){
 		$output = "<ul class=\"subjects\">";
 		$subject_set = find_all_subjects(); 
 		while($subject = mysqli_fetch_assoc($subject_set)){
 			$output .= "<li";
-			if($subject["id"] == $subject_id){
+			if($subject_array && $subject["id"] == $subject_array["id"]){
 				$output .= " class=\"selected\"";
 			}
 			$output .= ">";
@@ -89,7 +104,7 @@
 			$output .= "<ul class=\"pages\">";
 			while($page = mysqli_fetch_assoc($page_set)){ 
 				$output .= "<li";
-				if($page["id"] == $page_id){
+				if($page_array && $page["id"] == $page_array["id"]){
 					$output .= " class=\"selected\"";
 				}
 				$output .= ">";
--- Widget_Corp/public/manage_content.php
+++ Widget_Corp/public/manage_content.php
@@ -1,35 +1,22 @@
   <?php require_once("../includes/db_connection.php") ?>
   <?php require_once("../includes/functions.php") ?>
   <?php include("../includes/layouts/header.php") ?>
-
-  <?php 
-      if(isset($_GET["subject"])){
-       $selected_subject_id = $_GET["subject"];
-       $selected_page_id = null;	
-     }elseif (isset($_GET["page"])) {
-       $selected_page_id = $_GET["page"];
-       $selected_subject_id = null;
-     }else{
-       $selected_page_id = null;
-       $selected_subject_id = null;
-     }
- ?>
+  <?php find_selected_page();?>
 
  <div id="main">
    <div id="navigation">
-    <?php echo navigation($selected_subject_id, $selected_page_id); ?>	
+    <?php echo navigation($current_subject, $current_page); //here $current_subject or $current_page may be associative array or null.?>	
   </div>
   <div id="page">
-    <?php if($selected_subject_id){ ?>
+    <?php if($current_subject){ ?>
      <h2>Manage Subject</h2>
-      <?php $current_subject = find_subject_by_id($selected_subject_id); ?>
       Menu name: <?php echo $current_subject["menu_name"]; ?><br>
 
-    <?php } elseif($selected_page_id){ ?>
-     <h2>Manage Subject</h2>
-      <?php $current_page = find_page_by_id($selected_page_id); ?>
+    <?php } elseif($current_page){ ?>
+     <h2>Manage Page</h2>
+   
       Menu name: <?php echo $current_page["menu_name"]; ?><br>
-      <?php echo $selected_page_id; ?>
+
     <?php } else { ?>
       please select a subject or a page.
     <?php } ?> 
