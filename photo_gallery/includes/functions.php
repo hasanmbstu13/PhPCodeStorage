32d7823a25a74aa948577fcefeec5da36e829011
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-08-17 07:45:56 +0600

    Successfully created all assets and directories of photo gallery project.

---
+++ photo_gallery/includes/functions.php
@@ -0,0 +1,27 @@
+<?php 
+	
+	function strip_zeros_from_date($marked_string=""){
+		//remove the marked zeros
+		$no_zeros = str_replace('*0', '', $marked_string);
+		//remove any remaining marks
+		$cleaned_string = str_replace('*', '', $no_zeros);
+		return $cleaned_string;
+	}
+
+	function redirect_to($location = NULL){
+		if($location != NULL){
+			header("Location: " . $new_location);
+			exit;
+		}
+	}
+
+	function output_message($message=""){
+		if(!empty($message)){
+			return "<p class=\"message\">{$message}</p>";
+		} else {
+			return "";
+		}
+	}
+
+	//passwprd: GRANT ALL PRIVILEGES ON photo_gallery.* TO 'gallery'@'localhost' IDENTIFIED BY 'php0TL123';
+?>
