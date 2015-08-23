33dc28787785c188294d88b7c94e2d62f9f34aa7
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-08-23 07:51:05 +0600

    Session class is completed.

--- photo_gallery/includes/session.php
+++ photo_gallery/includes/session.php
@@ -7,11 +7,49 @@
 
 class Session {
 
+	private $logged_in=false;
+	public  $user_id;
+
 	function __construct() {
 		session_start();
+		$this->check_login(); // This method is useful to check the person is already logged in or not.
+		if($this->logged_in){
+			// actions to take right away if user is logged in
+		} else {
+			// actions to take right away if user is not logged in
+		}
+	}
+
+	public function is_logged_in() { // is logged in access this class class will set the attribute value.
+		return $this->logged_in;
+	}
+
+	public function login($user){
+		// database should find user based on username/password
+		if($user){
+			$this->user_id = $_SESSION['user_id'] = $user->id;
+			$this->logged_in = true;
+		}
+	}
+
+	public function logout() {
+		unset($_SESSION['user_id']);
+		unset($this->user_id);
+		$this->logged_in = false;
+	}
+
+	private function check_login() {
+		if(isset($_SESSION['user_id'])) {
+			$this->user_id 		= $_SESSION['user_id'];
+			$this->logged_in 	= true;
+		} else {
+			unset($this->user_id);
+			$this->logged_in = false;
+		}
 	}
 
 }
 
 $session = new Session();
+
 ?>
