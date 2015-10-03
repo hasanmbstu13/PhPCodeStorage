8a653604c88c4de510fd4a41f35e4b8b13280249
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-10-03 07:43:46 +0600

    Complete User CRUD.

--- photo_gallery/includes/initialize.php
+++ photo_gallery/includes/initialize.php
@@ -7,8 +7,8 @@
 // (\ for windows, / for Unix)
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define('SITE_ROOT','D:'.DS.'xampp'.DS.'htdocs'.DS.'PhPCodeStorage'.DS.'photo_gallery');
-// defined('SITE_ROOT') ? null : define('SITE_ROOT','C:'.DS.'xampp'.DS.'htdocs'.DS.'PhPCodeStorage'.DS.'photo_gallery');
+// defined('SITE_ROOT') ? null : define('SITE_ROOT','D:'.DS.'xampp'.DS.'htdocs'.DS.'PhPCodeStorage'.DS.'photo_gallery');
+defined('SITE_ROOT') ? null : define('SITE_ROOT','C:'.DS.'xampp'.DS.'htdocs'.DS.'PhPCodeStorage'.DS.'photo_gallery');
 
 defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'includes');
 
---
+++ photo_gallery/public/admin/test.php
@@ -0,0 +1,21 @@
+<?php 
+require_once("../../includes/initialize.php");
+if(!$session->is_logged_in()) { redirect_to("login.php");}
+?>
+
+<?php include_layout_template('admin_header.php'); ?>
+
+<?php 
+	// $user = new User();
+	// $user->username = "johnsmith";
+	// $user->password = "123";
+	// $user->first_name = "John";
+	// $user->last_name = "Smith";
+	// $user->create();
+	
+	$user = User::find_by_id(3);
+	$user->password = "789";
+	$user->save();
+?>	
+
+<?php include_layout_template('admin_footer.php'); ?>
