a6d6fcf908f9e9791f00624ac22426e8a6f08111
hasanmbstu13 <hasanmbstu13@gmail.com>
2015-11-05 16:36:34 +0600

    Files file are placed in the file folder.

--- photo_gallery/public/index.php
+++ photo_gallery/public/index.php
@@ -1,9 +1,13 @@
+<?php  ?>
 <?php require_once("../includes/initialize.php"); ?>
 <?php
 	// Find all photos
+	// calling static methods find_all()
 	$photos = Photograph::find_all(); 
+	// print_r($photos);
 ?>
 <?php include_layout_template('header.php'); ?>
+
 <?php foreach ($photos as $photo): ?>
   <div style="float: left; margin-left: 20px;">
 	<a href="photo.php?id=<?php echo $photo->id; ?>">
@@ -12,6 +16,7 @@
   	<p><?php echo $photo->caption; ?> </p>
   </div>
 <?php endforeach; ?>
+
 <?php include_layout_template('footer.php'); ?>
 
 
